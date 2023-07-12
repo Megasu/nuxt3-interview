c8ab23cc86b5ae02d55e83175a4f5d77d3bbee24
Mega Su <Megasu@qq.com>
2023-07-12 14:53:43 +0800

    feat: 面经详情-动态路由和参数获取

---
+++ pages/detail/[id].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// 获取页码参数
+const { params } = useRoute()
+
+console.log(params)
+</script>
+
+<template>
+  <h1>文章详情页 {{ $route.params }}</h1>
+</template>
+
+<style scoped></style>
